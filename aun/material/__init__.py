bcff04d93f9324b6140dbcccd3f74f4757f36288
lyjdwh <1412511544@qq.com>
2017-09-14 22:52:11 +0800

    seperate the material system from aun

--- aun/material/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Blueprint
-
-material = Blueprint('material', __name__, url_prefix='/material',
-                     static_folder='../static/material', template_folder='../templates/material')
-
-from . import views
--- aun/views.py
+++ aun/views.py
@@ -13,6 +13,6 @@ def index_test(path):
 
     """
     basedir = current_app.config['BASEDIR']
-    path = os.path.join(basedir, 'aunet/templates/home/index.html')
+    path = os.path.join(basedir, 'aun/templates/home/index.html')
     with open(path, 'r', encoding='utf-8') as response:
         return response.read()
